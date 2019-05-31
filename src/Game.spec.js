47a9f069604c532bf106e88a64b4d046ed45761b
Michal Pasierbski <michal.pasierbski@beekeeper.io>
2019-05-31 22:07:24 +0200

    add Game, Cell specs

--- src/Game.spec.js
+++ src/Game.spec.js
@@ -1,11 +1,36 @@
-// import Game from './Game';
+import Game from './Game';
+import Cell from './Cell';
 
 describe('Game', function() {
-  describe('#add', function() {
+  let game;
 
+  beforeEach(function() {
+    game = new Game();
   });
 
-  describe('#remove', function() {});
+  describe('#add', function() {
+    it('should add cell to plain', function() {
+      const cell = new Cell([0, 0]);
+
+      game.add(cell);
+
+      expect([...game].indexOf(cell)).toBeGreaterThan(-1);
+    });
+  });
+
+  describe('#remove', function() {
+    const cell = new Cell([9, 8], 1);
+
+    beforeEach(function() {
+      game.add(cell);
+    });
+
+    it('should remove cell from game', function() {
+      game.remove(cell);
+
+      expect([...game].indexOf(cell)).toEqual(-1);
+    });
+  });
 
   describe('#reset', function() {
   });
